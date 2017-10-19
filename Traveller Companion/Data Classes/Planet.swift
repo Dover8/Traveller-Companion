2115898e725371d40754030b590fd6657443d7ec
Ben MacKinnon <bilmackinnon@googlemail.com>
2017-10-19 15:13:54 +0100

    Set up basic planet class
    Need the constructor to actually construct yet.

---
+++ Traveller Companion/Data Classes/Planet.swift
@@ -0,0 +1,28 @@
+//
+//  Planet.swift
+//  Traveller Companion
+//
+//  Created by Ben MacKinnon on 19/10/2017.
+//  Copyright © 2017 Ben MacKinnon. All rights reserved.
+//
+
+import Foundation
+
+
+class Planet  {
+    
+    var Starport: Int;
+    var Size: Int;
+    var Atmosphere: Int;
+    var Hydrophonics: Int;
+    var Population: Int;
+    var Government: Int;
+    var Law: Int;
+    var Tech: Int;
+    
+    init(uwp: String) {
+        
+        
+    }
+    
+}
