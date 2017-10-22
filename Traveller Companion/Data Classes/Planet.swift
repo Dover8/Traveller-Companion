6c8a66beaa167e9bfe1fcf70b5dbc45def320ac5
Ben MacKinnon <bilmackinnon@googlemail.com>
2017-10-22 14:52:56 +0100

    Set up enum for trade codes.
    And began building them into the Planet data structure

--- Traveller Companion/Data Classes/Planet.swift
+++ Traveller Companion/Data Classes/Planet.swift
@@ -8,17 +8,38 @@
 
 import Foundation
 
+enum TradeCode {
+    case Ag //Agricultural
+    case As //Asteroid
+    case Ba //Barren
+    case De //Desert
+    case Fl //Fluid Ocean
+    case Ga //Garden
+    case Hi //High Population
+    case Ht //High Tech
+    case IC //Ice Capped
+    case In //Industrial
+    case Lo //Low Population
+    case Lt //Low Tech
+    case Na //Non-Agricultural
+    case NI //Non-Industrial
+    case Po //Poor
+    case Ri //Rich
+    case Va //Vacume
+    case Wa //Water world
+};
 
 class Planet  {
     
-    var Starport: UInt8;
-    var Size: UInt8;
-    var Atmosphere: UInt8;
-    var Hydrophonics: UInt8;
-    var Population: UInt8;
-    var Government: UInt8;
-    var Law: UInt8;
+    var Starport: UInt8; // Range = 0 - 12. Value is NOT hexidecimal
+    var Size: UInt8; //Range = 0 - 10
+    var Atmosphere: UInt8; // Range = 0 - 15(F)
+    var Hydrophonics: UInt8; // Range = 0 (desert) - 10 (entirely water)
+    var Population: UInt8; // Range = 0 - 12(C) (10 ^ value represents Population)
+    var Government: UInt8; // Range = 0 - 13(D)
+    var Law: UInt8; // Range = 0 - 9+ (No upper limit as Government level is added to the roll in generation).
     var Tech: UInt8;
+    var TradeCodes: [TradeCode];
     
     init?(uwp: String) {
         if uwp.validateUWP() {
@@ -36,6 +57,10 @@ class Planet  {
             print("Error parsing UWP")
             return nil
         }
+    }
+    
+    func CalculateTradeCodes() -> [TradeCode] {
+        var codes = []
     }
     
 }
